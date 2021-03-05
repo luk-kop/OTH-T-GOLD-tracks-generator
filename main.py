ddf330ce4b09718b62ab516b7eb894ee37d225cc
luk-kop <luk.kopicki@gmail.com>
2021-03-05 22:03:20 +0100

    GOLD tracks with random data.

--- main.py
+++ main.py
@@ -6,33 +6,36 @@ from ctc_data import CtcFlag, CtcType, CtcCategory
 
 
 class IdGenerator:
-    def __init__(self):
-        self.n = 1
+    """
+    Simple ID generator.
+    """
+    def __init__(self, start=1):
+        self.id = start
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        result = self.n
-        self.n += 1
-        return result
+        current_id = self.id
+        self.id += 1
+        return current_id
 
 
 class GoldCtc:
     """
-
+    A class representing CTC OTG set.
     """
-    id = IdGenerator()
+    name_id = IdGenerator()
 
     def __init__(self, name='TEST'):
         self.track_number = f'T{random.randrange(100, 99_999):05d}'
-        self.class_name = f'UNEQUATED-{name}{next(GoldCtc.id)}'     # to change
+        self.class_name = f'UNEQUATED-{name}{next(GoldCtc.name_id)}'     # to change
         self.trademark = ''
         self.type = CtcType.get_random_name()
         self.category = CtcCategory.get_random_name()
         self.pennant_number = f'{random.choice(["D", "A", "M", "F", "P", ""])}{random.randrange(10, 9_999)}'
         self.flag = CtcFlag.get_random_name()
-        self.sconum =''
+        self.sconum = ''
         self.selective_id = ''
         self.alert_code = random.choice(['', 'TGT', 'SUS', 'HIT', 'NSP'])
         while True:
@@ -59,31 +62,32 @@ class GoldCtc:
 
 
 class GoldXpos:
-    def __init__(self):
+    """
+    A class representing XPOS OTG set.
+    """
+    def __init__(self, position=None):
         self._date_time_group = self.date_time_group
         self.month_year = f'{datetime.utcnow().strftime("%b").upper()}{datetime.utcnow().strftime("%y")}'
-        self._position = self.position
-        self.sensor_code = ''
+        self.position = position
+        self.sensor_code = random.choice(['', 'RADAR', 'VISUAL', 'UNK', 'PHOTO', 'SRN25', 'IFF', 'IR'])
         self.bearing_of_major_axis = ''
         self.length_of_semi_major_axis = ''
         self.length_of_semi_minor_axis = ''
-        self.course = ''
-        self.speed = ''
+        self.course = f'{random.randint(0, 359):03d}.{random.randint(0, 9)}T'
+        self.speed = f'{random.randint(0, 50)}KTS'
         self.altitude = ''
         self.depth = ''
         self.rdf_rf = ''
-        self.source_code = ''
+        self.source_code = random.choice(['', 'EST', 'NTDS', 'UNK'])
         self.seq_contact_id = ''
         self.photos = ''
         self.total_no_of_contacts = ''
 
     @property
     def date_time_group(self):
-        # TODO: tracks from different timestamps
+        # TODO: different timestamp for each track
         # Substract 30 min from utcnow
         date = f'{(datetime.utcnow().replace(second=0) - timedelta(hours=0, minutes=30)).strftime("%d%H%M%S")}'
-        # Calculate checksum
-        # checksum = sum([int(num) for num in date]) % 10
         return f'{date}Z{self._check_sum(date)}'
 
     @property
@@ -91,14 +95,21 @@ class GoldXpos:
         """
         Returns position in format - LL:552521N0-0163311E5
         """
-        # TODO: Units in radius from position ()
-        latitude = f'{random.randrange(0, 90):02d}{random.randrange(0, 60):02d}{random.randrange(0, 60):02d}'
-        longitude = f'{random.randrange(0, 180):03d}{random.randrange(0, 60):02d}{random.randrange(0, 60):02d}'
-        latitude_dir = random.choice(['N', 'S'])
-        longitude_dir = random.choice(['E', 'W'])
-        return f'LL:{latitude}{latitude_dir}{self._check_sum(latitude)}-' \
-               f'{longitude}{longitude_dir}{self._check_sum(longitude)}'
+        # TODO: If more than one track and not random position - units in some radius from position
+        # TODO: Validate position with regex on input
+        return self._position
 
+    @position.setter
+    def position(self, value):
+        if not value:
+            latitude = f'{random.randrange(0, 90):02d}{random.randrange(0, 60):02d}{random.randrange(0, 60):02d}'
+            longitude = f'{random.randrange(0, 180):03d}{random.randrange(0, 60):02d}{random.randrange(0, 60):02d}'
+            latitude_dir = random.choice(['N', 'S'])
+            longitude_dir = random.choice(['E', 'W'])
+            position = f'LL:{latitude}{latitude_dir}{self._check_sum(latitude)}-' \
+                       f'{longitude}{longitude_dir}{self._check_sum(longitude)}'
+            self._position = position
+        # TODO: custom position
 
     @staticmethod
     def _check_sum(value):
@@ -108,17 +119,17 @@ class GoldXpos:
         return sum([int(num) for num in value]) % 10
 
     def get_all_attr(self):
-        print(self.__dict__)
         return [value for value in self.__dict__.values()]
 
     def __str__(self):
         separator = '/'
-        # return f'XPOS/{separator.join(self.get_all_attr())}'.rstrip(separator)
         return f'XPOS/{separator.join(self.get_all_attr())}'.rstrip(separator)
 
 
 class GoldTrack:
-
+    """
+    A class representing a single track
+    """
     def __init__(self):
         self.ctc = GoldCtc()
         self.xpos = GoldXpos()
@@ -129,18 +140,18 @@ class GoldTrack:
 
 class GoldMessage:
     """
-    Class represents GOLD msg to send.
+    A class representing GOLD msg to send.
     """
     msg_id = 0
 
-    def __init__(self, msg_originator='GOLDTX'):
-        self.gold_tracks = []
+    def __init__(self, track_count=1, msg_originator='GOLDTX'):
+        self.gold_tracks_count = track_count
         self.msg_originator = msg_originator    # TODO: 1-14 chars validation
         GoldMessage.msg_id_increase()
 
-    def add_gold_track(self, gold_track):
-        self.gold_tracks.append(gold_track)
-        return True
+    @property
+    def gold_tracks(self):
+        return [GoldTrack() for _ in range(self.gold_tracks_count)]
 
     @property
     def msg_header(self):
@@ -175,15 +186,10 @@ class GoldMessage:
     def __len__(self):
         """
         Returns number of GOLD tracks in message.
-        :return: int
         """
         return len(self.gold_tracks)
 
  
 if __name__ == "__main__":
-    msg = GoldMessage()
-    for _ in range(4):
-        test_msg = GoldTrack()
-        msg.add_gold_track(test_msg)
-
+    msg = GoldMessage(track_count=10)
     print(msg)
